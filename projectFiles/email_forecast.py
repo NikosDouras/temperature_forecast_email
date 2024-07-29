0bce4a97c1d55736e5ba8d128c6b869049ea50f7
Nikos Douras <139460420+NikosDouras@users.noreply.github.com>
2024-07-29 17:02:37 +0300

    added some logging for debugging

--- projectFiles/email_forecast.py
+++ projectFiles/email_forecast.py
@@ -10,6 +10,14 @@ load_dotenv()  # Load environment variables from .env file
 
 MY_EMAIL = os.getenv('GMAIL')    # Replace with your email address
 MY_PASSWORD = os.getenv('GMAIL_PASSWORD')          # Replace with your password
+
+
+if not MY_EMAIL:
+    print("getting env vars failed")
+else:
+    print(f"Loaded GMAIL: {MY_EMAIL}")
+    print(f"Loaded GMAIL_PASSWORD: {MY_PASSWORD}")
+    
 MY_LATITUDE = 40.6401
 MY_LONGITUDE = 22.9444
 URL = f"https://api.open-meteo.com/v1/forecast?latitude={MY_LATITUDE}&longitude={MY_LONGITUDE}&daily=weathercode,temperature_2m_max&timezone=Europe%2FBerlin"
@@ -32,6 +40,9 @@ def send_daily_email():
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(MY_EMAIL, MY_PASSWORD)
+            print("Login successful")
+    except Exception as e:
+        print(f"Login failed: {e}")
             for client in clients:
                 message = f"Subject: Today's temperature {temp}C\n\nThe highest temperature today is going to be {temp}C. Enjoy your day."
                 connection.sendmail(MY_EMAIL, client, message)
