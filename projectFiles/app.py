3008772f0040adfa0beb70be507fd277b1fdbf28
Nikos Douras <139460420+NikosDouras@users.noreply.github.com>
2024-08-06 11:54:37 +0300

    Update app.py

--- projectFiles/app.py
+++ projectFiles/app.py
@@ -22,5 +22,5 @@ def thank_you():
     return render_template('thank_you.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
